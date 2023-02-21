bdda80415ba5479cde86380d9e62ef352371f75f
Alanood0 <98521867+Alanood0@users.noreply.github.com>
2023-02-21 14:47:02 +0530

    Add files via upload

---
+++ src/Section.js
@@ -0,0 +1,30 @@
+function Section(){
+
+    return(
+    
+    <section>
+<div className="container-fluid hero-content">
+      <div className="row align-items-center">
+        <div className="col-md-6 col-sm-12 text-center" data-aos="fade-up">
+          <h1> Welcome to sharjah islamic bank </h1>
+          <br/>
+          <p className="lead muted"> 
+            An exclutive banking experinces built to serve the needs of a community to move,
+            <br/>
+            Delivering creative solution based on islamic principles of partnership and trust. </p>
+          <br/>
+          <br/>
+          <a className="get-started" href="./aboutus.html"> Discover more about US!</a>
+        <div/>
+        <div className="col-md-6 col-sm-12 text-center" data-aos="fade-left">
+          <div className="hero-img-div">
+            <img className="hero-img" src="https://assets.wam.ae/uploads/2019/04/452998792362513302.jpg" alt="img-showing-money-transfer"/>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+</section>
+);
+}
+export default Section;
